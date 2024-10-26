9383ace2f375240d136a59679f24402bcb6d1cf2
rpiontik <r.piontik@mail.ru>
2024-10-26 15:34:05 +0300

    Updated UI slot interface

--- interfaces/ui.ts
+++ interfaces/ui.ts
@@ -2,12 +2,20 @@ import { IDocHubUIComponent } from './uicomponents';
 
 export enum DocHubUISlot {
     avatar = 'avatar',
-    toolbar = 'toolbar'
+    toolbar = 'toolbar',
+    explorer = 'explorer'
 }
 
+export interface IDocHubUISlotOptions {
+    title?: string;
+}
+
+/**
+ * Интерфейс управления UI компонентами для предопределенных слотов
+ */
 export interface IDocHubUI {
     // Регистрирует UI компонент в слоте
-    register(slot: DocHubUISlot, component: IDocHubUIComponent);
+    register(slot: DocHubUISlot, component: IDocHubUIComponent, options?: IDocHubUISlotOptions);
     // Возвращает зарегистрированный компонент по слоту
     get(slot: string): IDocHubUIComponent[];
 }
