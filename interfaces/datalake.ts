19006bbf1becda02f41048b9448978673b6e01a5
rpiontik <r.piontik@mail.ru>
2024-11-05 08:22:00 +0300

    Added isContainsFile to DataLake API

--- interfaces/datalake.ts
+++ interfaces/datalake.ts
@@ -32,6 +32,8 @@ export interface IDataLakeChange {
     data: object | string | number | boolean;
     // Комментарии в манифест, где изменения будут зафиксированы
     comment: string;
+    // URI целевого файла для изменения
+    targetFile?: string;
 }
 
 export interface IDocHubPullDataParams  {
@@ -75,102 +77,137 @@ export interface IDocHubDataLake {
      * @returns             - Объект транзакции
      */
     beginTransaction(): Promise<IDocHubTransaction>;
+
     /**
      * Отправляет транзакцию в DataLake
      * @param transaction   - Объект транзакции
      */
     commitTransaction(transaction: IDocHubTransaction): Promise<IDocHubTransaction>;
+
     /**
      * Отменяет транзакцию
      * @param transaction   - Объект транзакции
      */
     rollbackTransaction(transaction: IDocHubTransaction): Promise<IDocHubTransaction>;
+
     /**
      * Возвращает URI текущего корневого манифеста
      * @returns             - URI корневого манифеста
      */
     getRootManifest(): string;
+
     /**
      * Монтирует произвольный файл в DataLake
      * @param uri   - URI монтируемого файла
      */
     mount(uri: string);
-    // Демонтирует файл из DataLake
-    //  uri         - URI демонтируемого файла
+
+    /**
+     * Демонтирует файл из DataLake
+     * @param uri               - URI демонтируемого файла
+     */
     unmount(uri: string);
-    // Требует перезагрузки файла задействованных в озере данных
-    //  uriPattern  - Шаблон проверки соответствия URI файла
-    //                Если undefined - перезагружает все
+
+    /**
+     * Требует перезагрузки файлов задействованных в озере данных
+     * @param uriPattern        - Шаблон проверки соответствия URI файла
+     *                            Если undefined - перезагружает все  
+     */
     reload(uriPattern?: string | string[] | RegExp);
+
+    /**
+     * Проверяет, что DataLake содержит файл
+     * @param uri               - URI файла или шаблон поиска
+     * @returns                 - Возвращает массив найденных файлов
+     */
+    isContainsFile(pattern: string | RegExp): Promise<string[]>;
+
     /**
      * Внесение изменений в DataLake
-     * @param changes   - Массив изменений вносимых в DataLake
-     * @param fileURI   - Целевой файл для изменений. Если он не указан, система сама его определяет
-     * @returns         - Массив выполненных преобразований
+     * @param changes           - Массив изменений вносимых в DataLake
+     * @returns                 - Массив выполненных преобразований
      */
-    pushData(changes: IDataLakeChange[], fileURI?: string): Promise<IDataLakeChange[]>;
-    // Получение данных из DataLake
-    //  expression  - JSONata выражение
-    //  params      - коллекция параметров (необязательно)
-    //  context     - контекст исполнения запроса (необязательно)
+    pushData(changes: IDataLakeChange[]): Promise<IDataLakeChange[]>;
+
+    /**
+     * Выполняет запрос к DataLake
+     * @param expression        - JSONata выражение
+     * @param params            - Коллекция параметров (необязательно)
+     * @param context           - Контекст исполнения запроса (необязательно)
+     * @returns                 - Результат выполненного запроса
+     */
     pullData(expression: string, params?: IDocHubPullDataParams, context?: any): Promise<any>;
-    // Сохраняет файла в DataLake
-    //  uri         - URI файла во внутреннем формате DocHub
-    //  context     - данные, которые требуется сохранить как файл
-    // В результате возвращает статус выполненного запроса
+
+    /**
+     * Сохраняет файла в DataLake
+     * @param uri               - URI файла во внутреннем формате DocHub
+     * @param content           - Содержимое файла
+     * @returns                 - Статус выполненного запроса
+     */
     pushFile(uri: string, content: any): Promise<any>;
-    // Получает файла из DataLake
-    //  uri         - URI файла во внутреннем формате DocHub
-    // В результате возвращает статус выполненного запроса
+
+    /**
+     * Загружает файл из DataLake
+     * @param uri               - URI файла во внутреннем формате DocHub
+     * @returns                 - Результат
+     */
     pullFile(uri: string): Promise<any>;
-    // Возвращает конечный URI на основании массива относительных и прямых URI
+
+    /**
+     * Возвращает конечный URI на основании массива относительных и прямых URI
+     * @param uri               - Массив URI
+     * @returns                 - Результирующий URI
+     */
     resolveURI(...uri: string[]): string;
+
     /**
      * Регистрирует редактор файлов
-     * @param pattern       - RegExp contentType файла. Например: ^.*\/markdown($|;.*$)
-     * @param component     - VUE компонент для редактирования файла
-     * @param title         - Название редактора файла
+     * @param pattern           - RegExp contentType файла. Например: ^.*\/markdown($|;.*$)
+     * @param component         - VUE компонент для редактирования файла
+     * @param title             - Название редактора файла
      */
     registerFileEditor(pattern: string, component: IDocHubFileEditorComponent, title?: string);
+
     /**
      * Возвращает массив зарегистрированных редакторов файлов 
-     * @returns             - Массив зарегистрированных редакторов объектов
+     * @returns                 - Массив зарегистрированных редакторов объектов
      */
     fetchFileEditors(): Promise<IDocHubFileEditorItem[]>;
+
     /**
      * Возвращает актуальный редактор для файла по contentType
-     * @param contentType   - Тип контента. Например: text/markdown
+     * @param contentType       - Тип контента. Например: text/markdown
      */
     getFileEditor(contentType: string): Promise<IDocHubFileEditorItem | null>;
 
     /**
      * Запрос на открытие файла на пользовательское редактирование. Необязательно будет выполнен.
      * Если редактор уже открыт, активирует его.
-     * @param uri           - URI файла 
-     * @param context       - Контекст редактирования файла. Необходим для связных редакторов и конструкторов.
-     * @returns             - Компонент редактора, если открытие оказалось успешным
+     * @param uri               - URI файла 
+     * @param context           - Контекст редактирования файла. Необходим для связных редакторов и конструкторов.
+     * @returns                 - Компонент редактора, если открытие оказалось успешным
      */
     openFileEditor(uri: string, context?: IDocHubFileEditorContext): Promise<IDocHubFileEditorComponent>;
 
     /**
      * Запрос на завершение пользовательского редактирования файла. Не обязательно будет выполнен.
-     * @param uri           - URI файла
-     * @returns             - true, если закрытие оказалось успешным
+     * @param uri               - URI файла
+     * @returns                 - true, если закрытие оказалось успешным
      */
     closeFileEditor(uri: string): Promise<boolean>;
 
     /**
      * Регистрирует соответствие шаблона файла типу контента.
      * Зарегистрированная связь является приоритетной по отношению к возвращаемому заголовку content-type сервера.
-     * @param pathPattern   - RegEx полного пути к файлу. Например: \.md$
-     * @param contentType   - Тип контента, который соответствует шаблону файла. Например: text/markdown
+     * @param pathPattern       - RegEx полного пути к файлу. Например: \.md$
+     * @param contentType       - Тип контента, который соответствует шаблону файла. Например: text/markdown
      */
     registerFileContentType(pattern: string, contentType: string);
 
     /**
      * Возвращает тип контента по названию файла
-     * @param file          - Полный путь к файлу. Например: README.md
-     * @returns             - Тип контента. Например: text/markdown
+     * @param file              - Полный путь к файлу. Например: README.md
+     * @returns                 - Тип контента. Например: text/markdown
      */
     getContentTypeForFile(file: string): string | null;
 }
