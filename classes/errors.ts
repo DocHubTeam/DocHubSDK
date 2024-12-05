3d99839664f4aec4987d5abd718241d940ed9de7
rpiontik <r.piontik@mail.ru>
2024-12-05 12:07:33 +0300

    Added problems interfaces & added problem error class

--- classes/errors.ts
+++ classes/errors.ts
@@ -1,3 +1,6 @@
+import { DocHub } from '..';
+import { DataLakePath } from '../interfaces/datalake';
+import { IDocHubProblem } from '../interfaces/problems';
 /**
  * Класс внутренних ошибок DocHub
  */
@@ -5,4 +8,25 @@ export class DocHubError extends Error {
     constructor(...params) {
       super(...params);
     }
+}
+/**
+ * Класс ошибок, являющихся проблемами для разрешения пользователем
+ */
+export class DocHubProblem extends DocHubError implements IDocHubProblem {
+  uid: string;
+  title: string;
+  description: string;
+  location?: string | undefined;
+  path?: DataLakePath | DataLakePath[] | undefined;
+  correction?: string | undefined;
+  constructor(data: IDocHubProblem) {
+    super(`Registered problem [${data.uid}]: ${data.title}`, { cause: data });
+    this.uid = data.uid;
+    this.title = data.uid;
+    this.description = data.description;
+    this.location = data.location;
+    this.path = data.path;
+    this.correction = data.correction;
+    DocHub.problems.emit(this);
+  }
 }
--- interfaces/problems.ts
+++ interfaces/problems.ts
@@ -1,4 +1,86 @@
+import { DataLakePath } from "./datalake";
+import { IDocHubDataSetProfile } from "./datasets";
+
+export type IDocHubProblemUID = string;
+export type IDocHubValidatorUID = string;
+
+/**
+ * Валидаторы проблем
+ */
+export interface IDocHubValidator extends IDocHubDataSetProfile {
+    /**
+     * Идентификатор валидатора
+     */
+    uid: IDocHubValidatorUID;
+    /**
+     * Название валидатора
+     */
+    title: string;
+}
+/**
+ * Исключения для регистрации проблем
+ */
+export interface IDocHubException {
+    /**
+     * Идентификатор проблемы, которая считается исключением
+     */
+    uid: IDocHubProblemUID;
+    /**
+     * Причина исключения
+     */
+    reason: string;
+}
+/**
+ * Зарегистрированная проблема
+ */
+export interface IDocHubProblem {
+    /**
+     * Уникальный идентификатор выявленной проблемы
+     */
+    uid: IDocHubProblemUID; 
+    /**
+     * Краткое описание / название проблемы
+     */
+    title: string;
+    /**
+     * Описание возникшей проблемы
+     */
+    description: string;
+    /**
+     * URI выявленной проблемы
+     */
+    location?: string;
+    /**
+     * Путь к проблемному объекту/ам в datalake
+     */
+    path?: DataLakePath | DataLakePath[];
+    /**
+     * Предложения о том, как исправить проблему
+     */
+    correction?: string;
+}
+
 export interface IDocHubProblems {
-    // Регистрирует ошибку в системе
+    /**
+     * Очищает список зарегистрированных проблем
+     */
+    clean();
+    /**
+     * Запускает процесс анализа проблем
+     */
+    review();
+    /**
+     * Регистрирует проблему в системе
+     * @param problem       - 
+     * @param title 
+     * @param uid 
+     */
     emit(problem: Error, title?: string, uid?: string);
+    /**
+     * Регистрирует проблему в системе
+     * @param problem 
+     * @param title 
+     * @param uid 
+     */
+    emit(problem: IDocHubProblem);
 }
