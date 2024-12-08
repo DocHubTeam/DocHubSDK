8c05a7d68963c8ffec019d5681775c6344f5c6de
rpiontik <r.piontik@mail.ru>
2024-12-08 14:18:19 +0300

    Added class editor proto component

---
+++ classes/vue2/Editor.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Prop, Watch, Component } from 'vue-property-decorator';
+import { DocHubComponentProto } from './Components';
+import { DocHubEditorContext } from '../..';
+
+@Component
+export class DocHubEditorProto extends DocHubComponentProto {
+  /**
+   * Контекст редактирования
+   */
+  @Prop({
+    type: Object,
+    default: null
+  }) readonly context: DocHubEditorContext | null;
+}
