d61151865fe38826a1c7b103e16899b4a856965f
rpiontik <r.piontik@mail.ru>
2025-01-05 13:03:03 +0300

    Added DocHubJSONataQueryObject class

--- interfaces/datasets.ts
+++ interfaces/datasets.ts
@@ -7,13 +7,65 @@ export type DocHubDataSetID =  string;
  */
 export type DocHubDataSetData = object;
 /**
- * JSONata запрос
+ * JSONata запрос с расширенными свойствами
  */
-export type DocHubJSONataQuery =  `(${string})` | `jsonata(${string})`;
+
+/**
+ * Простое, строковое определение JSONata запроса
+ */
+export type DocHubJSONataQuerySimple = `(${string})` | `jsonata(${string})`;
+
 /**
  * Ссылка на JSONata файл
  */
-export type DocHubJSONataFile =  `${string}.jsonata`;
+export type DocHubJSONataFile = `${string}.jsonata`;
+
+/**
+ * Метаинформация JSONata запроса
+ */
+export interface DocHubJSONataQueryMeta {
+    /**
+     * URI ресурса, где декларирован запрос 
+     */
+    source?: string;
+    /**
+     * Путь к свойству определения
+     */
+    path?: string;
+}
+
+/**
+ * Класс, позволяет создавать запросы с расширенной информацией об источнике его происхождения
+ * для удобства отладки.
+ */
+export class DocHubJSONataQueryObject extends String {
+    /**
+     * URI ресурса, где определен запрос
+     */
+    private uri: string | undefined;
+    /**
+     * Путь к определению запроса
+     */
+    private path: string | undefined;
+    constructor(query: DocHubJSONataQuerySimple, uri?: string, path?: string) {
+        super(query);
+        this.uri = uri;
+        this.path = path;
+        return this;
+    }
+    get __uri__(): string | undefined {
+        return this.uri;
+    }
+    get __path__(): string | undefined {
+        return this.path;
+    }
+}
+
+/**
+ * JSONata запрос
+ */
+export type DocHubJSONataQuery =  DocHubJSONataQuerySimple | DocHubJSONataQueryObject;
+
 /**
  * Ссылка на файл данных
  */
