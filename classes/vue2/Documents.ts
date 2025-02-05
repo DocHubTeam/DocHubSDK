1550ca83c656ddee4533f3ca59b5291f8e0f33ca
rpiontik <r.piontik@mail.ru>
2025-02-05 17:35:36 +0300

    Change target to location openEditor method of documents

--- classes/vue2/Documents.ts
+++ classes/vue2/Documents.ts
@@ -97,7 +97,7 @@ export class DocHubDocumentProto extends DocHubComponentProto implements IDocHub
    */
   openEditor(uri?: string): void {
     this.baseURI && DocHub.dataLake.openFileEditor(uri || this.baseURI, {
-      targetPath: this.profile.$base,
+      location: this.profile.$base.toString(),
       targetWindow: (window.event as any)?.ctrlKey ? DocHubUITargetWindow._blank : DocHubUITargetWindow._self
     });
   }
