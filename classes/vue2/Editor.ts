daeb30389a64b14582f1d7ee391c5e02950a009d
rpiontik <r.piontik@mail.ru>
2025-04-15 14:48:50 +0300

    Added autosave fixture to prototype editor

--- classes/vue2/Editor.ts
+++ classes/vue2/Editor.ts
@@ -4,8 +4,43 @@ import { DocHubComponentProto } from './Components';
 import { DocHub, EditorEvents } from '../..';
 import type { DocHubEditorContext } from '../..';
 
+type DocHubRoutePermitter = (to, from) => Promise<boolean>;
+
+class DocHubEditorExitPermitter {
+  private permitters: DocHubRoutePermitter[] = [];
+
+  constructor(...params) {
+    DocHub.router.registerMiddleware({
+      beforeEach: async(to, from, next): Promise<void> => {
+        debugger;
+      }
+    });
+  }
+
+  addPermitter(permitter: DocHubRoutePermitter) {
+      this.permitters.push(permitter);
+  }
+
+  removePermitter(permitter: DocHubRoutePermitter) {
+    this.permitters = this.permitters.filter((item) => item !== permitter);
+  }
+
+  async onBeforeEach(to, from, next) {
+      for(const permitter of this.permitters) {
+          const result = await permitter(to, from);
+          if (!result) {
+              // eslint-disable-next-line no-console
+              console.warn(`Editor prohibited transition to [${to.fullPath}].`);
+              next(false);
+          }
+      }
+      next();
+  }
+}
+
 @Component
 export class DocHubEditorProto extends DocHubComponentProto {
+  static permitter: DocHubEditorExitPermitter;
   /**
    * Контекст редактирования
    */
@@ -16,15 +51,18 @@ export class DocHubEditorProto extends DocHubComponentProto {
 
   constructor(...params) {
     super(...params);
+    DocHubEditorProto.permitter ||= new DocHubEditorExitPermitter();
     this.$on(EditorEvents.save, this.onSave);
     this.$on(EditorEvents.saveAs, this.onSaveAs);
     this.$on(EditorEvents.goto, this.onGoTo);
+    DocHubEditorProto.permitter.addPermitter(this.beforeExit);
   }
 
   destroyed(): void {
     this.$off(EditorEvents.save, this.onSave);
     this.$off(EditorEvents.saveAs, this.onSaveAs);
     this.$off(EditorEvents.goto, this.onGoTo);
+    DocHubEditorProto.permitter.removePermitter(this.beforeExit);
   }
 
   /**
@@ -45,5 +83,15 @@ export class DocHubEditorProto extends DocHubComponentProto {
    */
   async onSaveAs(uri: string): Promise<void>  {
     console.warn('Save as function is not implemented', uri);
+  }
+  /**
+   * Обрабатываем любой переход требуя автозапись
+   * @param to    - роут куда происходит переход
+   * @param from  - роут откуда происходит переход
+   * @returns     - true если переход разрешен, иначе - false
+   */
+  async beforeExit(to, from): Promise<boolean> {
+    await this.onSave();
+    return true;
   }
 }
